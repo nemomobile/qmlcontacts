8bbf1c7464b1881a35e5ded6f56c4f566d027bb6
Kaitlin Rupert <kaitlin.rupert@intel.com>
2011-06-10 11:14:20 -0700

    BMC#16093: ASCII names should be sorted after JPN names
    Japanese locales, sort ASCII names after Japanese names
    
    Signed-off-by: Kaitlin Rupert <kaitlin.rupert@intel.com>

--- lib/localeutils.cpp
+++ lib/localeutils.cpp
@@ -125,9 +125,10 @@ bool LocaleUtils::initCollator(int collType, QString locale)
 
     QLocale::Country country = getCountry();
     if ((country == QLocale::DemocraticRepublicOfKorea) ||
-        (country == QLocale::RepublicOfKorea)) {
+        (country == QLocale::RepublicOfKorea) || (country == QLocale::Japan)) {
 
-        //ASCII characters should be sorted after KOR characters
+        //ASCII characters should be sorted after
+        //non-ASCII characters for some languages
         UnicodeString rules = mColl->getRules();
         rules += "< a,A< b,B< c,C< d,D< e,E< f,F< g,G< h,H< i,I< j,J < k,K"
                  "< l,L< m,M< n,N< o,O< p,P< q,Q< r,R< s,S< t,T < u,U< v,V"
